986094c6e1e8664f599eb4b530257cd25ec08ee8
nshores <nick@shoresmedia.com>
2023-05-23 23:39:52 -0700

    various updates

--- packer/scripts/ubuntu_post_tasks.sh
+++ packer/scripts/ubuntu_post_tasks.sh
@@ -1,15 +1,26 @@
 apt-get install -f -y python net-tools curl python3-pip zsh
 
 # Install VMWare Guestinfo Cloud-init source
-curl -sSL https://raw.githubusercontent.com/vmware/cloud-init-vmware-guestinfo/master/install.sh | sh -
+#Should not be needed anymore as this is included with the latest version of cloud-init
+# curl -sSL https://raw.githubusercontent.com/vmware/cloud-init-vmware-guestinfo/master/install.sh | sh -
 
 
 # Reset Cloud-init state (https://stackoverflow.com/questions/57564641/openstack-Packer-cloud-init)
 cloud-init clean -s -l
 
 #allow cloud-init to customize network settings
-sudo rm /etc/cloud/cloud.cfg.d/99-installer.cfg
-sudo rm /etc/cloud/cloud.cfg.d/subiquity-disable-cloudinit-networking.cfg
+https://askubuntu.com/questions/1366315/terraform-cloud-init-via-extra-config-datasourcevmware
+sudo rm /etc/cloud/cloud.cfg.d/90_dpkg.cfg
+
+ 
+cat > /etc/cloud/cloud.cfg.d/99-vmware-guest-customization.cfg <<EOF
+disable_vmware_customization: false
+datasource:
+  VMware:
+    allow_raw_data: false
+    vmware_cust_file_max_wait: 10
+EOF
+
 
 #enable cloud-init customizations
 sudo echo "disable_vmware_customization: false" >> /etc/cloud/cloud.cfg
